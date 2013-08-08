ebdbcbdc10a07cf19510ed4375e12fb907168442
mastero9017 <lukilapaj@interia.pl>
2013-08-08 16:13:45 +0300

    Update clock-7x30.c
    Little overclock with Adreno 205 Specs

--- arch/arm/mach-msm/clock-7x30.c
+++ arch/arm/mach-msm/clock-7x30.c
@@ -1287,7 +1287,7 @@ static struct rcg_clk grp_2d_clk = {
 	.c = {
 		.dbg_name = "grp_2d_clk",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP2(NOMINAL, 192000000, HIGH, 245760000),
+		VDD_DIG_FMAX_MAP2(NOMINAL, 245760000, HIGH, 299520000),
 		CLK_INIT(grp_2d_clk.c),
 		.depends = &axi_grp_2d_clk.c,
 	},
@@ -1307,7 +1307,7 @@ static struct rcg_clk grp_3d_src_clk = {
 	.c = {
 		.dbg_name = "grp_3d_src_clk",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP2(NOMINAL, 192000000, HIGH, 245760000),
+		VDD_DIG_FMAX_MAP2(NOMINAL, 192000000, HIGH, 353280000),
 		CLK_INIT(grp_3d_src_clk.c),
 		.depends = &axi_li_grp_clk.c,
 	},
